6ccf0ac452afcaa8db85f14145d33cd80b20b560
Thong Quang <picothieu@gmail.com>
2023-03-27 14:29:39 +0700

    migrate DB to RDS

--- Server/Land-Vision/Data/DataContext.cs
+++ Server/Land-Vision/Data/DataContext.cs
@@ -5,19 +5,19 @@ namespace Land_Vision.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) :base(options){ }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         public DbSet<Account> Accounts { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<City> Citys { get; set; }
         public DbSet<District> Districts { get; set; }
         public DbSet<Street> Streets { get; set; }
-        public DbSet<Category> Categorys { get; set; }
+        public DbSet<Category> Categories { get; set; }
         public DbSet<Position> Positions { get; set; }
         public DbSet<Property> Properties { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<Status> Statuses { get; set; }
-        public DbSet <Image> Images { get; set; }
+        public DbSet<Image> Images { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
