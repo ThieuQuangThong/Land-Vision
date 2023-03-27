b38f60b31417f74af1afa93c94e2c3a57caae01b
Blade <minhuy123344@gmail.com>
2023-03-27 14:38:16 +0700

    add send email service

--- Server/Land-Vision/Controllers/WeatherForecastController.cs
+++ Server/Land-Vision/Controllers/WeatherForecastController.cs
@@ -1,3 +1,5 @@
+using Land_Vision.DTO;
+using Land_Vision.Interface.IServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Land_Vision.Controllers
@@ -6,6 +8,7 @@ namespace Land_Vision.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        private readonly IEmailService _emailService;
         private static readonly string[] Summaries = new[]
         {
         "Lanh VLz", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "DaumA"
@@ -13,21 +16,18 @@ namespace Land_Vision.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IEmailService emailService)
         {
             _logger = logger;
+            _emailService = emailService;
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public  IActionResult Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            var message = new Message(new string[] { "minhuy123344@gmail.com" }, "Test email", "This is the content from our email.");
+            _emailService.SendMail(message);
+            return Ok();
         }
     }
 }
--- Server/Land-Vision/service/EmailService.cs
+++ Server/Land-Vision/service/EmailService.cs
@@ -1,30 +1,58 @@
-using System.Net.Mail;
+using Land_Vision.DTO;
 using Land_Vision.Interface.IServices;
+using MimeKit;
 
 namespace Land_Vision.service
 {
     public class EmailService : IEmailService
     {
-        public async Task<bool> SendMail(string _from, string _to, string _subject, string _body, SmtpClient client)
+
+        private readonly EmailConfiguration _emailConfig;
+        public EmailService(EmailConfiguration emailConfig)
         {
-            MailMessage message = new MailMessage (
-                from: _from,
-                to: _to,
-                subject: _subject,
-                body: _body
-            );
-            message.BodyEncoding = System.Text.Encoding.UTF8;
-            message.SubjectEncoding = System.Text.Encoding.UTF8;
-            message.IsBodyHtml = true;
-            message.ReplyToList.Add (new MailAddress (_from));
-            message.Sender = new MailAddress (_from);
-            try {
-                await client.SendMailAsync (message);
-                return true;
-            } catch (Exception ex) {
-                Console.WriteLine (ex.Message);
-                return false;
+            _emailConfig = emailConfig;
+        }
+
+        private MimeMessage CreateEmailMessage(Message message)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress("LandVision",_emailConfig.From));
+            emailMessage.To.AddRange(message.To);
+            emailMessage.Subject = message.Subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = string.Format("<h2 style='color:red;'>{0}</h2>", message.Content) };
+            return emailMessage;
+        }
+
+        private void Send(MimeMessage mailMessage)
+        {
+            using (var client = new MailKit.Net.Smtp.SmtpClient())
+            {
+                try
+                {
+                    client.Connect(_emailConfig.SmtpServer, _emailConfig.Port, true);
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    client.Authenticate(_emailConfig.UserName, _emailConfig.Password);
+                    client.Send(mailMessage);
+                }  
+                catch
+                {
+                    //log an error message or throw an exception or both.
+                    throw;
+                }
+                finally
+                {
+                    client.Disconnect(true);
+                    client.Dispose();
+                }
             }
         }
+
+        public void SendMail(Message message)
+        {
+            var emailMessage = CreateEmailMessage(message);
+            Send(emailMessage);
         }
+
+
     }
+}
