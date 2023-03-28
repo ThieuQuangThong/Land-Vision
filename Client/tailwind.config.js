a5af9c4e37973ab39b3bbe7466e28eeb1667e2c2
Bao Nhat <baonhat20@gmail.com>
2023-03-28 14:25:20 +0700

    add forrgot pass

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -5,10 +5,7 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'hero-pattern': "url('https://i.pinimg.com/564x/6e/8c/73/6e8c734fac3015053de5314d6c4bec9e.jpg')",
-        'footer-texture': "url('/img/footer-texture.png')"
-    },
+     
   },
   plugins: [],
   }
