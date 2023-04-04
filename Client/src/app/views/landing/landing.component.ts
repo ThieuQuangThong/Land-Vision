8e636da6514b5ff7a5a6ef33fad6af160b850b1e
Blade <minhuy123344@gmail.com>
2023-04-04 13:52:05 +0700

    load more funtion

--- Client/src/app/views/landing/landing.component.ts
+++ Client/src/app/views/landing/landing.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { PostService } from "src/app/_service/post.service";
 import { PagingModel } from "src/app/models/paging-model";
-import { PostResponeModel } from "src/app/models/post-respone-model";
+import { PostModel } from "src/app/models/post-model";
 
 @Component({
   selector: "app-landing",
@@ -10,23 +10,37 @@ import { PostResponeModel } from "src/app/models/post-respone-model";
 
 })
 export class LandingComponent implements OnInit {
-  paging: PagingModel = new PagingModel();
-  postRespone: PostResponeModel = new PostResponeModel();
+  paging: PagingModel = {
+    skipCount : 0,
+    maxResultCount : 1,
+  }
+  isFullItem: boolean = false;
+  postRespone: PostModel[] = [];
   constructor(private postService:PostService) {
-
   }
 
   ngOnInit(): void {
-    this.paging = {
-      skipCount : 0,
-      maxResultCount : 12,
-    }
-    this.postService.getAllPost(this.paging)
+    this.getPost(this.paging);
+  }
+
+  getPost(paging: PagingModel){
+    this.postService.getAllPost(paging)
     .subscribe(
       respone =>{
-        this.postRespone = respone;
+        var {skipCount, maxResultCount} = respone.pagination;
+
+        this.postRespone = [...this.postRespone, ...respone.listItem];
+        paging.skipCount = skipCount + maxResultCount;
+
+        if(paging.skipCount >= respone.totalCount){
+          this.isFullItem = true;
+        }
       }
     )
+  }
+
+  loadMore(){
+    this.getPost(this.paging);
   }
 
 }
