74e603f0e7398d2d804ef3f50d74840da25aef37
Blade <minhuy123344@gmail.com>
2023-04-13 10:50:24 +0700

    feat: change direction type

--- Server/Land-Vision/Migrations/20230413014359_add_direction.Designer.cs
+++ Server/Land-Vision/Migrations/20230413014359_add_direction.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Land_Vision.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Land_Vision.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230413014359_add_direction")]
+    partial class add_direction
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -126,6 +129,9 @@ namespace Land_Vision.Migrations
                         .IsRequired()
                         .HasColumnType("longtext");
 
+                    b.Property<int>("Direction")
+                        .HasColumnType("int");
+
                     b.Property<bool>("IsVerified")
                         .HasColumnType("tinyint(1)");
 
@@ -170,8 +176,9 @@ namespace Land_Vision.Migrations
                     b.Property<int>("CategoryId")
                         .HasColumnType("int");
 
-                    b.Property<int>("Direction")
-                        .HasColumnType("int");
+                    b.Property<string>("Direction")
+                        .IsRequired()
+                        .HasColumnType("longtext");
 
                     b.Property<double>("FrontangeArea")
                         .HasColumnType("double");
---
+++ Server/Land-Vision/Migrations/20230413014818_remove_direction.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Land_Vision.Migrations
+{
+    /// <inheritdoc />
+    public partial class remove_direction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Direction",
+                table: "Posts");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Direction",
+                table: "Properties",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Direction",
+                table: "Properties",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Direction",
+                table: "Posts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
