1ba3cc6f2eda6eecdfdbe22ee156b5b0237e33e3
Bao Nhat <baonhat20@gmail.com>
2023-04-18 17:15:59 +0700

    fix header

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -5,8 +5,52 @@ module.exports = {
   ],
   theme: {
     extend: {
-     
+
   },
   plugins: [],
+  },
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: {"50":"#ecfdf5","100":"#d1fae5","200":"#a7f3d0","300":"#6ee7b7","400":"#34d399","500":"#10b981","600":"#059669","700":"#047857","800":"#065f46","900":"#064e3b"}
+      }
+    },
+    fontFamily: {
+      'body': [
+    'Inter',
+    'ui-sans-serif',
+    'system-ui',
+    '-apple-system',
+    'system-ui',
+    'Segoe UI',
+    'Roboto',
+    'Helvetica Neue',
+    'Arial',
+    'Noto Sans',
+    'sans-serif',
+    'Apple Color Emoji',
+    'Segoe UI Emoji',
+    'Segoe UI Symbol',
+    'Noto Color Emoji'
+  ],
+      'sans': [
+    'Inter',
+    'ui-sans-serif',
+    'system-ui',
+    '-apple-system',
+    'system-ui',
+    'Segoe UI',
+    'Roboto',
+    'Helvetica Neue',
+    'Arial',
+    'Noto Sans',
+    'sans-serif',
+    'Apple Color Emoji',
+    'Segoe UI Emoji',
+    'Segoe UI Symbol',
+    'Noto Color Emoji'
+  ]
+    }
   }
 }
