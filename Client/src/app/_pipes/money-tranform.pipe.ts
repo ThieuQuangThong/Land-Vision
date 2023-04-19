e84c12cb02f55956e9b260b61ee80af0f879af3d
Thong Quang <picothieu@gmail.com>
2023-04-19 17:14:31 +0700

    css home page

--- Client/src/app/_pipes/money-tranform.pipe.ts
+++ Client/src/app/_pipes/money-tranform.pipe.ts
@@ -9,9 +9,9 @@ export class MoneyTranformPipe implements PipeTransform {
     const billion = 1000000000;
     const million = 1000000;
     if (value >= billion) {
-      return (value / billion).toFixed(2) + ' Tỷ VNĐ';
+      return (value / billion).toFixed(2) + ' Tỷ';
     } else if (value >= million) {
-      return (value / million).toFixed(2) + ' Triệu VNĐ';
+      return (value / million).toFixed(2) + ' Triệu';
     } else {
       return value.toString() + ' VNĐ';
     }
