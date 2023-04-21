811167d2cafb0d5a759908c161e98092abc4ead9
Blade <minhuy123344@gmail.com>
2023-04-21 10:17:45 +0700

    feat: complete slice image

--- Client/src/app/components/post-image/post-image.component.ts
+++ Client/src/app/components/post-image/post-image.component.ts
@@ -12,6 +12,10 @@ export class PostImageComponent implements  OnInit {
   startDragPos: number = 0;
   distanNumber: number = 0;
   distanceString: string = '0px';
+  limitDrag: number = 0;
+
+  widthListImage: number = 0;
+  widthContain: number = 0;
 
   images: string[] =['https://file4.batdongsan.com.vn/resize/1275x717/2023/04/18/20230418145607-b8ca_wm.jpg',
                     'https://file4.batdongsan.com.vn/resize/1275x717/2023/04/18/20230418145607-d122_wm.jpg',
@@ -28,41 +32,74 @@ export class PostImageComponent implements  OnInit {
   }
   ngOnInit(): void {}
   ngAfterViewInit(): void {
-    const widthListImage =  this._elementRef.nativeElement.querySelector('#imageThumnails').offsetWidth;
-    const widthContain =  this._elementRef.nativeElement.querySelector('#imageThumnailContain').offsetWidth;
+    this.widthListImage =  this._elementRef.nativeElement.querySelector('#imageThumnails').offsetWidth;
+    this.widthContain =  this._elementRef.nativeElement.querySelector('#imageThumnailContain').offsetWidth;
+
+    this.limitDrag = this.widthListImage - this.widthContain;
   }
 
   previousImage(){
     if(this.currentImagePos - 1 < 0){
       this.currentImagePos = this.images.length - 1;
+      this.distanNumber = -(this.limitDrag+100)
+      this.distanceString = (this.distanNumber).toString() + 'px';
     }
     else{
       this.currentImagePos -= 1;
     }
-      this.currentImagePos -= 1;
+    const widthOfCurrentImagePos = (this.currentImagePos+1) * 110;
+    if(widthOfCurrentImagePos + this.distanNumber  < this.widthContain ){
+      if(this.distanNumber + 100 >= 0){
+        this.distanNumber = 0
+        this.distanceString = (this.distanNumber).toString() + 'px';
+        return
+      }
+      this.distanceString = (this.distanNumber +100).toString() + 'px';
+      this.distanNumber += 100;
+    }
+
+
   }
 
   afterImage(){
+
     if(this.currentImagePos+1 > this.images.length-1){
       this.currentImagePos = 0;
-      return;
+      this.distanNumber = 0
+      this.distanceString = (this.distanNumber).toString() + 'px';
+    }
+    else {
+      this.currentImagePos += 1;
     }
 
-    this.currentImagePos += 1;
+    const widthOfCurrentImagePos = (this.currentImagePos+1) * 110;
+    if(widthOfCurrentImagePos + this.distanNumber  > this.widthContain ){
+      if(this.distanNumber - 100 <= -this.limitDrag){
+        this.distanNumber = -this.limitDrag
+        this.distanceString = (this.distanNumber).toString() + 'px';
+        return
+      }
+      this.distanceString = (this.distanNumber -100).toString() + 'px';
+      this.distanNumber -= 100;
+    }
   }
+
   moveImages(e: any){
+
     const x = Number(e.clientX);
+
+
     if(x === 0){
       return;
     }
     const movedDistance = this.startDragPos - Number(e.clientX);
-    this.distanceString = (this.distanNumber - movedDistance).toString() + 'px';
-
-
-    // this.distanceString =  this.distanNumber.toString() + 'px';
-
-
+    const leftPos =  this.distanNumber - movedDistance;
+    if(leftPos <= -this.limitDrag ||  leftPos > 0){
+        return;
+    }
+    this.distanceString = leftPos.toString() + 'px';
   }
+
   endDrag(e:any){
     this.distanNumber =Number(this.distanceString.replace('px',''));
 
