9c19cd93def21e25f3a8816cb65d2c3666cf6979
Minh Huy <huynm@bkhightech.vn>
2023-04-25 00:16:54 +0700

    fgsdrgrd

--- Client/src/app/views/landing/landing.component.ts
+++ Client/src/app/views/landing/landing.component.ts
@@ -75,6 +75,9 @@ export class LandingComponent implements OnInit {
         if(paging.skipCount >= respone.totalCount){
           this.isFullItem = true;
         }
+        else{
+          this.isFullItem = false;
+        }
       },
       error => {
         this.isLoading = false
